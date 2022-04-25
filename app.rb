a3d2aced81474cae9fbceacaeb158d9605e35a7a
Tosie <minnaji@outlook.com>
2022-04-25 16:19:08 -0400

    assignment completed fiswip

---
+++ app.rb
@@ -0,0 +1,12 @@
+# app.rb
+# puts "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+# puts [1, 2, 3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts  "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
