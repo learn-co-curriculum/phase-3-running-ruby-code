c294c151efb7d311d72976820950ed08657de393
Mwenje <mwenje24@gmail.com>
2022-08-13 11:50:48 +0300

    commit tree 40ddef46e346ed372558308c970b0e9f9137c9b3

---
+++ app.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+
+print "Pass this test, please."
+
+puts [1, 2, 3].inspect
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
