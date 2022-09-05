c757bbb87fe2e4dfb7cf6edd99e3ca0f92b7a76e
Adelice <nancytoniw@gmail.com>
2022-09-05 10:22:28 +0200

    commit tree ac6a406a2c57150f682987c746c943eb02c16365

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
-dog_name="Lucy"
-puts "Say hello to my dog #{dog_name}"
+# print  "Hello world!"
+# print  "Hello world!"
+# print  "Hello world!"
+# puts add a line break which is different when you use print 
+# p is used to print or console numbers or other datatypes which are not strings 
+# puts [1, 2, 3]
+#using puts will change the array to a string using the .to_s method 
+
+#pp is used for nested arrays or hashes 
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts "Hello World!"
+print  "Pass this test, please."
+p [1,2,3]
