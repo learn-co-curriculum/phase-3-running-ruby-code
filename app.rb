a5209163534c8116cf869b18e53ba1fe92821c4e
Cecilia Milla <cmilla1020@gmail.com>
2022-12-04 12:55:13 -0500

    commit tree 163b38ad5c3177d3aba36d1213811d4aeac46fa7

---
+++ app.rb
@@ -0,0 +1,9 @@
+# pp [{ id: 1, hello: "World" }, 
+#     { id: 2, hello: "Ruby" }, 
+#     { id: 3, hello: "Moon" }, 
+#     { id: 4, hello: "Learner" }]
+
+# p [1,2,3]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
