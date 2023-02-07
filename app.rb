efc73af388116858d441b080b40229c77de9af71
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-07 10:40:30 +0300

    commit tree 0cce5ae5ebacb082d97abe2a234443a9d6a7837f

--- app.rb
+++ app.rb
@@ -1,23 +1,6 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
 
-# p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
-
-puts add(1,3) + add(2,2)
