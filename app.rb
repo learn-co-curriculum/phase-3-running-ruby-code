6be84ed1c295cf94caed6126dfad79ffd79b7031
rukia <ruqayashar1@gmail.com>
2023-05-16 00:32:31 +0300

    running ruby

--- app.rb
+++ app.rb
@@ -14,10 +14,7 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+puts "Hello World!"
 
-puts add(1,3) + add(2,2)
+print "Pass this test, please."
+p [1,2,3]
